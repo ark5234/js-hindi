7536c3ba7485283ab561c68bf337ff06eb1d0959
Vikrant Kawadkar <114129298+ark5234@users.noreply.github.com>
2024-12-27 18:45:56 +0000

    table creation

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,18 @@
-console.log("vikrant")
+const accountId = 144553
+let accountEmail = "vikrant.google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+//accountId = 2
+/*
+prefer not to use var
+because of isssues of scope and function 
+*/
+
+accountEmail = "vk@vk.com"
+accountPassword = "211211"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
