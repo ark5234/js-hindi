58be566ef1df2de11594e63501d13a9b4d7764d8
Vikrant Kawadkar <114129298+ark5234@users.noreply.github.com>
2024-12-28 16:25:55 +0000

    conversions of data types

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -7,11 +7,19 @@ let valueInNumber = Number(score)
 console.log(typeof valueInNumber );
 console.log(valueInNumber);
 */
+/*
 let isLoggedIn = undefined
 let booleanIsloggedIn = Boolean(isLoggedIn)
 
 console.log(booleanIsloggedIn);
 console.log(typeof booleanIsloggedIn)
 console.log(typeof isLoggedIn)
+*/
 
+let someNumber = undefined
 
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof someNumber)
+console.log(typeof stringNumber)
