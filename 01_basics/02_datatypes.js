b93bebae7554b20efeba6577fae2d06a719e9b40
Vikrant Kawadkar <114129298+ark5234@users.noreply.github.com>
2024-12-28 16:17:16 +0000

    01 update and o2 creation

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) // we are using node js not browser
+
+console.log(3+3)
+console.log("Vikrant")
+
+let name = "vikrant"
+let age= 20
+let loggedIn = true
+
+/* types of datatypes:
+number= 2^53
+bigint
+string= ""
+boolean= true/false
+null= standalone datatype
+undefined
+symbol= unique
+object
+
+*/
+
+console.log(typeof (2^34))
