3aeace05e182b56d2ab6ce3a327224e2c6594791
Vikrant Kawadkar <114129298+ark5234@users.noreply.github.com>
2024-12-28 16:16:14 +0000

    conversion operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,17 @@
+/*let score = "vikrant"
+
+console.log(typeof(score));
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber );
+console.log(valueInNumber);
+*/
+let isLoggedIn = undefined
+let booleanIsloggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsloggedIn);
+console.log(typeof booleanIsloggedIn)
+console.log(typeof isLoggedIn)
+
+
