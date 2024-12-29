005a941a837f16bbcd41e17069275a281fd6e67b
Vikrant Kawadkar <114129298+ark5234@users.noreply.github.com>
2024-12-29 18:26:04 +0000

    comparision

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,19 @@
+/*console.log(2>1);
+console.log(2>=1);
+console.log(2==1);
+*/
+
+/*
+console.log("5">3 );
+console.log("02">1);
+*/ 
+/*
+console.log(null>0);
+console.log(null==0); 
+console.log(null>=0); //comparision converts null to a number 0 
+*/
+// === checks datatype also while compairing
+
+console.log("2"===2);
+
+
---
+++ 01_basics/05_strings.js
@@ -0,0 +1,4 @@
+const name= "vikrant"
+const age =  20
+//console.log(name + age + " kawadakar");
+
