7aad2a85e537303b3ab578d62c43126931a53989
Vikrant Kawadkar <114129298+ark5234@users.noreply.github.com>
2024-12-29 19:20:48 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -2,3 +2,11 @@ const name= "vikrant"
 const age =  20
 //console.log(name + age + " kawadakar");
 
+console.log(`Hello my name is ${name} and my age is ${age}`);
+
+const gameName = new String('vikrantk')
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
