77316afb2da4a197b91d88f913c5801d64271157
Vikrant Kawadkar <114129298+ark5234@users.noreply.github.com>
2024-12-30 18:10:14 +0000

    string complete

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -10,3 +10,18 @@ console.log(gameName.__proto__);
 console.log(gameName.length);
 console.log(gameName.toUpperCase());
 console.log(gameName.charAt(3));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-6,4)
+console.log(anotherString);
+
+const newStringOne = "   vikrant    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "http://vikrant.com/vikrant%20kawadkar"
+console.log(url.replace('%20' , '-'))
+
+console.log(url.includes('vikrant'));
