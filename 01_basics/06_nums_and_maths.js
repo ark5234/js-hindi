242858df6be2700753d73755a1e1d5788acc56a4
Vikrant Kawadkar <114129298+ark5234@users.noreply.github.com>
2025-01-02 18:58:49 +0000

    nums

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,21 @@
+const score = 400
+console.log(score);
+
+const accountBalance = 4000
+const mrp = 754
+Gst = mrp*0.18
+
+balance = accountBalance -mrp -Gst
+
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(5));
+
+const otherNumber = 59.34745
+console.log(otherNumber.toPrecision(3));
+
+const hundreds =  10000
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+
